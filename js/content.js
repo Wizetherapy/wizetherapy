f264060214adbb5cfc68926172f20cbc878e5dfb
Wizetherapy <healing@wizetherapy.com>
2026-07-06 11:15:16 -0700

    Added javascripts to make text editing easier

---
+++ js/content.js
@@ -0,0 +1,23 @@
+const content = {
+
+invitation: 
+`ealing begins by creating a space where your story can be heard without judgment. Together, we'll explore the patterns, experiences, and strengths that have shaped your life.
+
+Together, we'll explore the patterns, experiences, and strengths that have shaped your life.`,
+
+practice: 
+`Therapy is a conversation rooted in curiosity, compassion, and respect. Rather than offering quick answers, we'll take the time to understand your unique experiences, helping you reconnect with your own wisdom and move toward meaningful change.`,
+
+journey: 
+`Therapy is a conversation rooted in curiosity, compassion, and respect. Rather than offering quick answers, we'll take the time to understand your unique experiences, helping you reconnect with your own wisdom and move toward meaningful change.
+Healing isn't about becoming someone different.
+It is about discovering who you have always been beneath fear, pain, and expectation.`,
+
+guidance: 
+`Healing begins by creating a space where your story can be heard without judgment. Together, we'll explore the patterns, experiences, and strengths that have shaped your life.
+My role is not to tell you who to become.
+My role is to walk beside you while you rediscover your own wisdom.`,
+
+begin:
+`If you're ready to begin therapy, I'd be honored to walk alongside you. Together, we'll create a space for healing, growth, and lasting change.`
+};
---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.getElementById("invitation-text").innerHTML += content.invitation.replace(/\n/g, "<br>");
+
+document.getElementById("practice-text").innerHTML = content.practice.replace(/\n/g, "<br>");
+
+document.getElementById("journey-text").innerHTML = content.journey.replace(/\n/g, "<br>");
+
+document.getElementById("guidance-text").innerHTML += content.guidance.replace(/\n/g, "<br>");
+
+document.getElementById("begin-text").innerHTML = content.begin.replace(/\n/g, "<br>");
