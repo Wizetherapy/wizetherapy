08fb52ec552d87ac717fc4af5c6e36839bde9eb7
Wizetherapy <healing@wizetherapy.com>
2026-07-07 08:13:42 -0700

    Updated description chapters

--- js/content.js
+++ js/content.js
@@ -1,9 +1,9 @@
 const content = {
 
 invitation: 
-`ealing begins by creating a space where your story can be heard without judgment. Together, we'll explore the patterns, experiences and strengths that have shaped your life. 
+`ealing begins by creating a space where your story can be heard without judgment. When you feel seen and understood, you remember that you matter and your life experience has value. 
 
-You have been lost and alone for too long. You need new ideas, fresh perspectives and trusted guidance to achieve the next stage of growth.`,
+Life is hard and no one should be navigting it alone. You may feel lost, confused, stressed and depressed. Maybe you've been repeating the same patterns for too long. You need new ideas, fresh perspectives and trusted guidance to achieve the next stage of growth.`,
 
 practice: 
 `Therapy is a conversation rooted in curiosity, compassion and respect. Rather than offering quick answers, we'll take the time to understand your unique experiences, helping you reconnect with your own wizdom and move toward meaningful change.
@@ -13,7 +13,7 @@ I work with Individuals and Couples to create meaning and purpose through life's
 journey: 
 `Healing isn't about becoming someone different. It is about discovering who you have always been beneath fear, pain and expectation. The key to unlocking your joy, ease and potential is becoming your own best friend. 
 
-It is through self love, acceptance and trust that you free yourself from definitions and beliefs that limit you. When you heal your relationship to your self first, everything else in your world mirrors that back. It's time to stop settling and prioritize your own well being.`,
+It is through self love, acceptance and trust that you free yourself from definitions and beliefs that limit you. When you heal your relationship to your self first, everything else in your world mirrors that back. It's time to stop settling and prioritize your personal transformation.`,
 
 guidance: 
 `I am the faithful torch carrier, illuminating the path as you rediscover your own wizdom. I teach you how to interpret your symptoms, access your intuition, make meaning of the wounds and find purpose in the pain. I show you how to integrate and embody the insights and wizdom into daily life. 
