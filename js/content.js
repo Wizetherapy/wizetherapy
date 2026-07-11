1a9b4a8b68d28fb421bebb5ed2570f5cc73c0647
Wizetherapy <healing@wizetherapy.com>
2026-07-11 10:20:23 -0700

    Updated content sections

--- js/content.js
+++ js/content.js
@@ -11,14 +11,14 @@ practice:
 I work with Individuals and Couples to create meaning and purpose through life's challenges. With honest self reflection, acountability and acceptance, you empower yourself to navigate anxiety, depression, trauma, relationships and self worth in new and lasting ways.`,
 
 journey: 
-`Healing isn't about becoming someone different. It is about discovering who you have always been beneath fear, pain and expectation. The key to unlocking your joy, ease and potential is becoming your own best friend. 
+`Healing isn't about becoming someone different. It is about discovering who you have always been beneath fear, pain and expectation. The key to unlocking joy, ease and potential is believing in your own mythic, hero's quest. 
 
 It is through self love, acceptance and trust that you free yourself from definitions and beliefs that limit you. When you heal your relationship to your self first, everything else in your world mirrors that back. It's time to stop settling and prioritize your personal transformation.`,
 
 guidance: 
 `I am the faithful torch carrier, illuminating the path as you rediscover your own wizdom. I teach you how to interpret your symptoms, access your intuition, make meaning of the wounds and find purpose in the pain. I show you how to integrate and embody the insights and wizdom into daily life. 
 
-I have been guiding souls through the healing process for 16 years utilizing mindfulness, cognitive-behavioral therapy, narrative therapy, attachment theory, emotionally-focused therapy and hypnotherapy. My goal for you is to make peace with your past and believe in your dreams.`,
+I have been guiding souls through the healing process for 16 years utilizing mindfulness, cognitive-behavioral therapy, narrative therapy, attachment theory, emotionally-focused therapy and hypnotherapy. My goal for you is to be whole in spirit, mind and body; being fearless toward pain and knowing deeply that your desires and visions are guiding your life in the right direction.`,
 
 begin:
 `If you're ready to begin therapy, I am honored to walk alongside you. Together, we'll create a space for healing, growth and lasting change.`
