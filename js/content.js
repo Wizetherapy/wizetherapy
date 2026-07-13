5f21483905b8a39f0f4afc6a82fd5b1332885a2e
Wizetherapy <healing@wizetherapy.com>
2026-07-13 16:00:13 -0700

    Highlighted keywords

--- js/content.js
+++ js/content.js
@@ -3,22 +3,22 @@ const content = {
 invitation: 
 `ealing begins by creating a space where your story can be heard without judgment. When you feel seen and understood, you remember that you matter and your life experience has value. 
 
-Life is hard and no one should be navigating it alone. You may feel lost, confused, stressed and depressed. Maybe you've been repeating the same patterns for too long. You need new ideas, fresh perspectives and trusted guidance to achieve the next stage of growth.`,
+Life is hard and no one should be navigating it alone. You may feel <span style="color: #871a1a;">lost, confused, stressed and depressed</span>. Maybe you've been <span style="color: #1536a3;">repeating the same patterns for too long.</span> When you are suffering from low self-steem and lacking healthy boundaries, you need support cultivating <span style="color: #306e01;">self-love, acceptance and trust.</span>`,
 
 practice: 
 `Therapy is a conversation rooted in curiosity, compassion and respect. Rather than offering quick answers, we'll take the time to understand your unique experiences, helping you reconnect with your own wizdom and move toward meaningful change.
 
-I work with Individuals and Couples to create meaning and purpose through life's challenges. With honest self reflection, accountability and acceptance, you empower yourself to navigate anxiety, depression, trauma, relationships and self worth in new and lasting ways.`,
+I work with <span style="color: #306e01;">Individuals ($225/50 mins)</span> and <span style="color: #306e01;">Couples ($350/80 mins)</span> in <span style="color: #871a1a;">California, Oregon and Washington</span> virtually to create meaning and purpose through life's challenges. With honest self reflection, accountability and acceptance, you empower yourself to navigate <span style="color: #1536a3;">anxiety, depression, transitions, relationships and self worth</span> in new and lasting ways.`,
 
 journey: 
-`Healing isn't about becoming someone different. It is about discovering who you have always been beneath fear, pain and expectation. The key to unlocking joy, ease and potential is believing in your own mythic hero's quest. 
+`Healing isn't about becoming someone different. It is about discovering who you have always been <span style="color: #871a1a;">beneath fear, pain and expectation.</span> The key to unlocking joy, ease and potential is believing in your own mythic hero's quest. 
 
-It is through self love, acceptance and trust that you free yourself from definitions and beliefs that limit you. When you heal your relationship to your self first, everything else in your world mirrors that back. It's time to stop settling and prioritize your personal transformation.`,
+It is through self love, acceptance and trust that you free yourself from definitions and beliefs that limit you. When you <span style="color: #1536a3;">heal your relationship to your self first,</span> everything else in your world mirrors that back. It's time to stop settling and prioritize your personal transformation.`,
 
 guidance: 
-`I am the faithful torch carrier, illuminating the path as you rediscover your own wizdom. I teach you how to interpret your symptoms, access your intuition, make meaning of the wounds and find purpose in the pain. I show you how to integrate and embody the insights and wizdom into daily life. 
+`I am Bronwyn Lee, the faithful torch carrier illuminating the path as you rediscover your own wizdom. I teach you how to interpret your symptoms, access your intuition, make meaning of the wounds and find purpose in the pain. I show you how to integrate and embody the insights and wizdom into daily life. 
 
-I have been guiding souls through the healing process for 16 years utilizing mindfulness, cognitive-behavioral therapy, narrative therapy, attachment theory, emotionally-focused therapy and hypnotherapy. My goal for you is to be whole in spirit, mind and body; being fearless toward pain and knowing deeply that your desires and visions are guiding your life in the right direction.`,
+ As a <span style="color: #871a1a;">Licensed Marraige and Family Therapist</span> (83314 & T1449) and a <span style="color: #1536a3;">Licensed Mental Health Clinician</span> (LH 60822228) I have been guiding souls through the healing process for 16 years utilizing <span style="color: #306e01;">mindfulness, cognitive-behavioral therapy, narrative therapy, attachment theory, emotionally-focused therapy and hypnotherapy.</span> My goal for you is to be whole in spirit, mind and body; being fearless toward pain and knowing deeply that your desires and visions are guiding your life in the right direction.`,
 
 begin:
 `If you're ready to begin therapy, I am honored to walk alongside you. Together, we'll create a space for healing, growth and lasting change.`
